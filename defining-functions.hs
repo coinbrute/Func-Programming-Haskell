66a572cd0ae10e582fba59ac94de49267fca39b6
Andrew Dufresne <adufres@Andrews-MacBook-Pro.local>
2022-01-29 16:35:10 -0700

    made small alignment change.

--- defining-functions.hs
+++ defining-functions.hs
@@ -16,8 +16,8 @@ abs :: Int -> Int
 abs n = if n >= 0 then n else (-n)
 
 signum :: Int -> Int 
-signum n = if n < 0 then (-1) else
-            if n == 0 then 0 else 1
+signum n = if n < 0  then (-1) else
+           if n == 0 then 0    else 1
 
 {-
                                     Guarded Equations
